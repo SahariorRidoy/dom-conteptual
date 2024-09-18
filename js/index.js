afeaacd411a2785819550ba62f47318a3d7474b9
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-19 00:48:50 +0600

    Added some style and using addEventListener

--- js/index.js
+++ js/index.js
@@ -1,17 +1,44 @@
+
+const plusBtn =document.getElementById('plusButton');
+plusBtn.style.backgroundColor='red'
+plusBtn.style.fontSize='20px'
+plusBtn.style.border='none'
+plusBtn.style.color='white'
+plusBtn.style.padding='15px'
+
+const minusB =document.getElementById('minusButton');
+minusB.style.backgroundColor='green'
+minusB.style.fontSize='20px'
+minusB.style.border='none'
+minusB.style.color='white'
+minusB.style.padding='15px'
+
 sum=0;
 function clickMe() {
     const h2=document.getElementById('counter');
     sum +=1;
     h2.innerText=sum;
 }
-function clickMeDecrement() {
-    const h2=document.getElementById('counter');
+// function clickMeDecrement() {
+//     const h2=document.getElementById('counter');
     
+//     if(sum>0){
+//         sum -=1;
+//     h2.innerText=sum;  
+//     }
+//     else {
+//          h2.innerText="Counter Cannot be negative values"
+//     }
+// }
+
+const minusBtn =document.getElementById('minusButton');
+minusBtn.addEventListener('click',function(){
+    const h2=document.getElementById('counter')
     if(sum>0){
         sum -=1;
-    h2.innerText=sum;  
+    h2.innerText=sum;
     }
-    else {
-         h2.innerText="Counter Cannot be negative values"
+    else{
+        h2.innerText="Counter Cannot be Negative Value"
     }
-}
+})
